4b8358b608afe292faa47e032c7d434be8da28f2
Benjay2K <benilepuschitz@gmail.com>
2022-05-09 13:37:51 +0200

    correction

--- Ex5.1.c
+++ Ex5.1.c
@@ -4,7 +4,8 @@ int main () {
     int a;
     float ∗p;
 
-    p = &a; //critical line, error: incompatible types when assigning to type 'float' from type 'int *'
+    //p = &a; //critical line, error: incompatible types when assigning to type 'float' from type 'int *'
+    p = (float*)&a;//cast as float
     ∗p = 3.1415;
 
     printf( "Pi = %d\n" , a);
@@ -12,3 +13,5 @@ int main () {
 
     return 0 ;
 }
+//c) interprets result as an integer even tough it's a float
+
